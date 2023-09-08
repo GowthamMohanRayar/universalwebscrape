5cfbfe99f83864ba7d815b517a944776f826c216
GowthamMohanRayar <53146105+GowthamMohanRayar@users.noreply.github.com>
2023-09-08 19:14:48 +0530

    initial commit

---
+++ src/main/java/com/web/universalwebscrape/UniversalwebscrapeApplication.java
@@ -0,0 +1,13 @@
+package com.web.universalwebscrape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalwebscrapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalwebscrapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/universalwebscrape/UniversalwebscrapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.universalwebscrape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalwebscrapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
